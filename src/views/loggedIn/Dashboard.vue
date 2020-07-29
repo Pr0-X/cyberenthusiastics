07e9107559e9dc6f1c1df0a925d0aaa95c141a69
Leo <leodahal4@gmail.com>
2020-07-29 07:54:19 +0545

    covered unpushed changes

--- src/views/loggedIn/Dashboard.vue
+++ src/views/loggedIn/Dashboard.vue
@@ -1,25 +1,25 @@
 <template>
-  <v-app>
-    <div>
-      <Navbar />
-      <v-container fluid>
-        <v-content>
-          <CreatePost />
-        </v-content>
-      </v-container>
-    </div>
-  </v-app>
+    <v-app>
+        <div>
+            <Navbar />
+            <v-container fluid>
+                <v-content>
+                    <CreatePost />
+                </v-content>
+            </v-container>
+        </div>
+    </v-app>
 </template>
 
 <script>
 import Navbar from "@/components/NavBar";
 import CreatePost from "../loggedIn/createPost";
 export default {
-  name: "Dashboard",
-  components: {
-    Navbar,
-    CreatePost
-  }
+    name: "Dashboard",
+    components: {
+        Navbar,
+        CreatePost
+    }
 };
 </script>
 
--- src/views/loggedIn/createPost.vue
+++ src/views/loggedIn/createPost.vue
@@ -2,48 +2,56 @@
     <v-container fluid>
         <v-row align="center">
             <v-col cols="12">
-            <v-row justify="space-between">
-                <v-card
-                    color="primary--text"
-                    outlined
-                    raised
-                    :width="widthForCard"
-                >
-                    <v-card-title>
-                        <v-row class="mx-5 my-2">
-                            <h2>{{ postOrBlog }}</h2>
-                            <h2 class="ml-4 mr-4">|</h2>
-                            <v-btn depressed class="white">Publish</v-btn>
-                            <v-spacer></v-spacer>
-                            <v-btn depressed class="white">Blog ?</v-btn>
-                        </v-row>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-col cols="8">
-                            <v-row>
-                                <v-text-field
-                                    label="Title of the post"
-                                    outlined
-                                ></v-text-field>
+                <v-row justify="space-between" class="mb-6" no-gutters>
+                    <v-card
+                        color="primary--text"
+                        outlined
+                        raised
+                        :width="widthForCard"
+                        class="pa-2"
+                        tile
+                    >
+                        <v-card-title>
+                            <v-row class="mx-5 my-2">
+                                <h2>{{ postOrBlog }}</h2>
+                                <h2 class="ml-4 mr-4">|</h2>
+                                <v-btn depressed class="white">Publish</v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn depressed class="white">Blog ?</v-btn>
                             </v-row>
-
-                            <v-row>
-                                <v-textarea
-                                    label="Description of the Post"
-                                    outlined
-                                >
-                                </v-textarea>
-                            </v-row>
-                        </v-col>
-                        <v-col cols="4" style="right: 0">
-                            <v-row>
-                                <v-text-field placeholder="tags">
-                                </v-text-field>
-                            </v-row>
-                        </v-col>
-                    </v-card-text>
-                </v-card>
-            </v-row>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-simple-table dense>
+                                <tr>
+                                    <td>
+                                        <v-text-field
+                                            label="Title of the post"
+                                            outlined
+                                        ></v-text-field>
+                                    </td>
+                                    <td style="float: right;">
+                                        <v-text-field
+                                            v-model="searchQuery"
+                                            label="Enter the Tags"
+                                            hint="Example: Python"
+                                            outlined
+                                            clearable
+                                        ></v-text-field>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <v-textarea
+                                            label="Description of the Post"
+                                            outlined
+                                        >
+                                        </v-textarea>
+                                    </td>
+                                </tr>
+                            </v-simple-table>
+                        </v-card-text>
+                    </v-card>
+                </v-row>
             </v-col>
         </v-row>
     </v-container>
@@ -61,5 +69,4 @@ export default {
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
